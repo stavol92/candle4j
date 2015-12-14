545e364d6dc6b86884721fe0080f5b33cc6f877d
vovnet <vovnet@yandex.ru>
2015-12-14 15:18:11 +0300

    add other math

--- Calculate.java
+++ Calculate.java
@@ -4,7 +4,36 @@ public class Calculate {
 		System.out.println("Hello! Calculating...");
 		int first = Integer.valueOf(arg[0]);
 		int second = Integer.valueOf(arg[1]);
-		int sum = first + second;
-		System.out.println("Result: " + sum);
+		int res = invol(first, second);
+		System.out.println("Result: " + res);
+	}
+	
+	// сложение
+	public static int add(int a, int b) {
+		return a + b;
+	}
+	
+	// вычитание
+	public static int sub(int a, int b) {
+		return a - b;
+	}
+	
+	// умножение
+	public static float multy(float a, float b) {
+		return a * b;
+	}
+	
+	// деление
+	public static double div(double a, double b) {
+		return a / b;
+	}
+	
+	// возведение в степень
+	public static int invol(int a, int b) {
+		int res = a;
+		for (int i = 1; i < b; i++) {
+			res *= a;
+		}
+		return res;
 	}
 }
