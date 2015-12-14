10a5ec941552341dd0403142de8ead88c4891ca8
vovnet <vovnet@yandex.ru>
2015-12-14 15:04:15 +0300

    Add sum

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,10 @@
 public class Calculate {
 	
 	public static void main(String[] arg) {
-		System.out.println("Hello");
+		System.out.println("Hello! Calculating...");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int sum = first + second;
+		System.out.println("Result: " + sum);
 	}
 }
